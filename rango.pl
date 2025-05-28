4b0c75561ea7a2cc84f8dc66f18e080ca6d87164
yael-gamah <yaelhernandezg123@gmail.com>
2025-05-28 00:51:24 -0600

    Rangos

---
+++ rango.pl
@@ -0,0 +1,8 @@
+% rango(Personaje, Rango).
+
+rango("Monkey D. Garp", "Vicealmirante").
+rango("Sengoku", "Almirante de Flota").
+rango("Sakazuki", "Almirante Jefe").
+rango("Borsalino", "Almirante").
+rango("Smoker", "Vicealmirante").
+rango("Koby", "Capitan").
